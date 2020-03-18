5c530d67af8ad82c64733c5ff2929a1e4d848e9e
cc <1141191703@qq.com>
2020-03-18 17:46:23 +0800

    '*'

--- CC-TF2.0/3. 张量前向传播/forward.py
+++ CC-TF2.0/3. 张量前向传播/forward.py
@@ -7,16 +7,20 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # x: [60k, 28, 28]
 # y: [60k]
-(x, y), _ = datasets.mnist.load_data()
+(x, y), (x_test, y_test) = datasets.mnist.load_data()
 # X:[0,255]->[0,1]
 x = tf.convert_to_tensor(x, dtype=tf.float32) / 255.
 y = tf.convert_to_tensor(y, dtype=tf.int32)
+
+x_test = tf.convert_to_tensor(x_test, dtype=tf.float32) / 255.
+y_test = tf.convert_to_tensor(y_test, dtype=tf.int32)
 
 print(x.shape, y.shape, x.dtype, y.dtype)
 print(tf.reduce_min(x), tf.reduce_max(x))
 print(tf.reduce_min(y), tf.reduce_max(y))
 
 train_db = tf.data.Dataset.from_tensor_slices((x, y)).batch(128)
+test_db = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(128)
 train_iter = iter(train_db)
 sample = next(train_iter)
 print('batch', sample[0].shape, sample[1].shape)
@@ -82,3 +86,29 @@ for epoch in range(10):     # 迭代数据集
 
         if step % 100 == 0:
             print(epoch, step, 'loss: ', float(loss))
+
+    # test
+    total_correct, total_num = 0, 0
+    for step, (x, y) in enumerate(test_db):
+        # [b,28,28] => [b, 289*28]
+        x = tf.reshape(x, [-1, 28*28])
+        h1 = tf.nn.relu(x@w1 + b1)
+        h2 = tf.nn.relu(h1@w2 + b2)
+        out = h2@w3 + b3
+
+        # out [b, 10] ~ R -> [0, 1]
+        prob = tf.nn.softmax(out, axis=1)
+        # int64!!
+        pred = tf.argmax(prob, axis=1)
+        pred = tf.cast(pred, dtype=tf.int32)
+        correct = tf.cast(tf.equal(pred, y), dtype=tf.int32)
+        correct = tf.reduce_sum(correct)
+
+        total_correct += int(correct)
+        total_num += x.shape[0]
+
+    acc = total_correct / total_num
+    print("test acc: ", acc)
+
+
+
